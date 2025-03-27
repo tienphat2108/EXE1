8fe03be9874df560fb253c6984905cf9d8d24a32
tienphat2108 <roghe2026@gmail.com>
2025-03-27 11:27:39 +0700

    Update serve

--- laravel/app/Http/Controllers/Controller.php
+++ laravel/app/Http/Controllers/Controller.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class UserController extends Controller
 {
-    //
+    public function login() {
+        return view('login');
+    }
+    
+    public function register() {
+        return view('register');
+    }
+    
+    public function updateUser() {
+        return view('update');
+    }
+    
+    public function listUser() {
+        return view('list');
+    }
+    
+    public function viewUser() {
+        return view('view');
+    }
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
+
+// Route mặc định của Laravel (giữ lại nếu bạn muốn)
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Các route cho UserController
+Route::get('/login', [UserController::class, 'login'])->name('login');
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::get('/update', [UserController::class, 'updateUser'])->name('update');
+Route::get('/list', [UserController::class, 'listUser'])->name('list');
+Route::get('/view', [UserController::class, 'viewUser'])->name('view');
